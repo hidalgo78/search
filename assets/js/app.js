666069621dc1c1c6493257cdc20d6c95098eb855
Fabian <efehidalgod@gmail.com>
2019-09-30 00:54:31 -0600

    wip new changes

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,50 +1,7 @@
 $(function() {
-    var search = $('.js-search');
-    search.on("click", function(event) {
-        var email = $('.js-email-s').val();
-        var found = false;
-        event.preventDefault();
-        if (ValidateEmail(email)) {
-          $('.fail-msg').fadeOut('fast');
-          $('.js-email-s').removeClass("fail");
-            $.ajax({
-                url: 'http://search.csdev1.com/data.json',
-                dataType: 'json',
-                success: function(data) {
-                    $.each(data, function(key, val) {
-                        $.each(val, function(key, val) {
-                            if (val.email === email) {
-                                //console.log(val);
-                                found = true;
-                                window.localStorage.setItem('data', JSON.stringify(val));
-                                window.location.href = "/results.html";
-                            }else {
-                              found = false;
-                            }
-                        });
-                    });
-                    if(found == false) {
-                      //localStorage.clear();
-                      console.log("no encontre ese correo");
-                      setTimeout(function(){
-                        $('.fail-msg').text('No info for this email address..Please try again!');
-                          $('.fail-msg').fadeIn('fast');
-                       }, 500);
-                      //  window.location.href = "http://localhost/BeenVerified/results.html";
-                    }
-                },
-                statusCode: {
-                    404: function() {
-                        alert('There was a problem with the server.  Try again soon!');
-                    }
-                }
-            });
-        }else {
-          $('.js-email-s').addClass('fail');
-          $('.fail-msg').text('Please add a valid email address');
-          $('.fail-msg').fadeIn('fast');
-        }
-    });
+    if (window.location.href.indexOf("results") > -1) {
+        loadData();
+    }
 
     function ValidateEmail(inputText) {
         var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
@@ -54,6 +11,53 @@ $(function() {
             return false;
         }
     }
+
+    var search = $('.js-search');
+    search.on("click", function(event) {
+        var email = $('.js-email-s').val();
+        var msg =   $('.fail-msg');
+        var found = false;
+        var searchInput = searchInput;
+        event.preventDefault();
+
+        if (ValidateEmail(email)) {
+
+            msg.fadeOut('fast');
+            searchInput.removeClass("fail");
+            $.ajax({
+                url: 'http://search.csdev1.com/data.json',
+                dataType: 'json',
+                success: function(data) {
+                    $.each(data, function(key, val) {
+                        $.each(val, function(key, val) {
+                            if (val.email === email) {
+                                found = true;
+                                window.localStorage.setItem('data', JSON.stringify(val));
+                                window.location.href = "/results.html";
+                            } else {
+                                found = false;
+                            }
+                        });
+                    });
+                    if (found == false) {
+                        setTimeout(function() {
+                            msg.text('No info for this email address..Please try again!');
+                            msg.fadeIn('fast');
+                        }, 500);
+                    }
+                },
+                statusCode: {
+                    404: function() {
+                        alert('There was a problem with the server.  Try again soon!');
+                    }
+                }
+            });
+        } else {
+            searchInput.addClass('fail');
+            msg.text('Please add a valid email address');
+            msg.fadeIn('fast');
+        }
+    });
 
     function loadData() {
         var dataFrom = JSON.parse(window.localStorage.getItem('data'));
@@ -72,17 +76,18 @@ $(function() {
         $('.js-email p').text(email);
 
         $.each(phones, function(key, val) {
-            $('.js-numbers').append("<a href='tel:" + val.phone + "'>" + val.phone + "</a>");
+            if (val.phone) {
+                $('.js-numbers').append("<a href='tel:" + val.phone + "'>" + val.phone + "</a>");
+            }else {
+                $('.js-relatives').append("<p>No info available</p>");
+            }
         });
         $.each(relatives, function(key, val) {
             if (val.name) {
                 $('.js-relatives').append("<p>" + val.name + "</p>");
+            }else {
+                $('.js-relatives').append("<p>No info available</p>");
             }
         });
-
-        console.log(dataFrom);
-    }
-    if (window.location.href.indexOf("results") > -1) {
-        loadData();
     }
 });
