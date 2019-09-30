f4f004a36976d816df8b700593dbb759646f3099
Fabian <efehidalgod@gmail.com>
2019-09-30 00:39:01 -0600

    wip bug in search

--- assets/js/app.js
+++ assets/js/app.js
@@ -29,7 +29,7 @@ $(function() {
                       setTimeout(function(){
                         $('.fail-msg').text('No info for this email address..Please try again!');
                           $('.fail-msg').fadeIn('fast');
-                       }, 3000);
+                       }, 1000);
                       //  window.location.href = "http://localhost/BeenVerified/results.html";
                     }
                 },
